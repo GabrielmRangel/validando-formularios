c1f32c1b68774693058ddb421fdc7bbe4241a198
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2023-05-12 20:10:18 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+import formataCpf from "./valida-cpf.js";
+
+const camposFormulario = document.querySelectorAll('[required]');
+
+camposFormulario.forEach((campo) => {
+    campo.addEventListener('blur', () => verificaCampo(campo));
+});
+
+function verificaCampo(campo){
+    if(campo.name == "cpf" && campo.value.length >= 11){
+        formataCpf(campo);
+    }
+}
